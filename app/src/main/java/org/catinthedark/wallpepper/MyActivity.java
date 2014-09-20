a097e432bfadc9de71950e2dcf3e48ec2115c440
Kirill <kolbasisha@gmail.com>
2014-09-20 10:37:08 +0700

    Store preferences.

--- app/src/main/java/org/catinthedark/wallpepper/MyActivity.java
+++ app/src/main/java/org/catinthedark/wallpepper/MyActivity.java
@@ -1,6 +1,7 @@
 package org.catinthedark.wallpepper;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -36,18 +37,38 @@ public class MyActivity extends Activity {
 
     public static final String TAG = "WallPepper";
 
+    private final String SHARED_PREFS_NAME = "wallpepper_sharedprefs";
+    private final String GROUP_ID_KEY = "group_id";
+    private final String RANDOM_RANGE_KEY = "random_range";
+
+    private EditText groupIdEditText;
+    private EditText randomRangeEditText;
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+        groupIdEditText = (EditText) findViewById(R.id.groupIdEditText);
+        randomRangeEditText = (EditText) findViewById(R.id.randomRangeEditText);
         Button setWallpaperButton = (Button) findViewById(R.id.setWallpaperButton);
+
+        preferences = getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE);
+        if (preferences.contains(GROUP_ID_KEY)) {
+            groupIdEditText.setText(preferences.getString(GROUP_ID_KEY, ""));
+        }
+
+        if (preferences.contains(RANDOM_RANGE_KEY)) {
+            randomRangeEditText.setText(String.valueOf(preferences.getInt(RANDOM_RANGE_KEY, 10)));
+        }
 
         setWallpaperButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String groupId = ((EditText) findViewById(R.id.groupIdEditText)).getText().toString();
-                int randomNumber = Integer.valueOf(((EditText) findViewById(R.id.randomCountEditText)).getText().toString());
+                String groupId = groupIdEditText.getText().toString();
+                int randomNumber = Integer.valueOf(randomRangeEditText.getText().toString());
+
                 String url = String.format("https://api.vk.com/method/photos.get?owner_id=-%s&album_id=wall&rev=1&count=%d", groupId, randomNumber);
 
                 new RequestTask().execute(url, getApplicationContext());
@@ -55,6 +76,14 @@ public class MyActivity extends Activity {
         });
     }
 
+    @Override
+    protected void onPause() {
+        preferences.edit()
+                .putInt(RANDOM_RANGE_KEY, Integer.valueOf(randomRangeEditText.getText().toString()))
+                .putString(GROUP_ID_KEY, groupIdEditText.getText().toString())
+                .apply();
+        super.onPause();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
